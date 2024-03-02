3cf3d6eb8d5f3ae573f0a8308dbde835195188b1
Enes <mail@enesimran.net>
2024-03-02 16:01:51 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Enes Imran Kilic on 02.03.24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
